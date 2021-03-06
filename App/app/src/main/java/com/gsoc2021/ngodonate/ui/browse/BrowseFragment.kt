af288de78107ab9fbdb7ca993a1f3bd3de82d613
Kartikey <pandeykh@mail.uc.edu>
2021-03-06 15:00:49 -0500

    Connected firestore to list view

--- App/app/src/main/java/com/gsoc2021/ngodonate/ui/browse/BrowseFragment.kt
+++ App/app/src/main/java/com/gsoc2021/ngodonate/ui/browse/BrowseFragment.kt
@@ -13,7 +13,6 @@ import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.gsoc2021.ngodonate.R
-import kotlinx.coroutines.tasks.await
 
 class BrowseFragment : Fragment() {
     
@@ -21,7 +20,7 @@ class BrowseFragment : Fragment() {
     /*var firebasedatabase: FirebaseDatabase? = null
     var NGOList: ArrayList<NGO>?  = null
     var ref: DatabaseReference? = null*/
-    val ngoList = ArrayList<NGO>()
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -29,82 +28,13 @@ class BrowseFragment : Fragment() {
     ): View? {
 
         val viewOfLayout = inflater.inflate(R.layout.fragment_browse, container, false)
-
         listView = viewOfLayout.findViewById(R.id.ngo_list_view)
-
-        val ngoList = NGO.getNGOsFromFile()
-        Log.d("browse","${ngoList.size}")
-
-        val adapter = NGOAdapter(this.requireContext(), ngoList)
-        listView.adapter = adapter
-
+        NGO.readData {
+            Log.d("browse","$it")
+            val adapter = NGOAdapter(this.requireContext(), it)
+            listView.adapter = adapter
+        }
         return viewOfLayout
     }
-
-    companion object {
-
-        private var db = Firebase.firestore
-        private lateinit var auth: FirebaseAuth
-        val docRef = db.collection("NGOs")
-        val documents: MutableList<DocumentSnapshot>? = null
-
-        suspend fun getNGOsFromFile(): ArrayList<NGO> {
-
-            val documents = getListOfNgos()
-
-            for(document in documents){
-                val email = document.get("email") as String
-                val current_ngo = NGO(
-                    document.get("name") as String,
-                    document.get("email") as String,
-                    document.get("phone_number") as String,
-                    document.get("hq_location") as String,
-                    document.get("volunteer") as String,
-                    document.get("website") as String
-                )
-                Log.d("EMAIL",  "$email" )
-                ngoList.add(current_ngo)
-            }
-
-
-            /*docRef.get()
-                .addOnCompleteListener { task ->
-                    if(task.isSuccessful)
-                    {
-                        for(document in task.result){
-                            val email = document.get("email") as String
-                            val current_ngo = NGO(
-                                document.get("name") as String,
-                                document.get("email") as String,
-                                document.get("phone_number") as String,
-                                document.get("hq_location") as String,
-                                document.get("volunteer") as String,
-                                document.get("website") as String
-                            )
-                            Log.d("EMAIL",  "$email" )
-                            ngoList.add(current_ngo)
-                        }
-                    }
-                }
-                .addOnFailureListener { exception ->
-                    Log.d("FAILURE", "get failed with ", exception)
-                }
-                   */
-
-            for(ngo in ngoList)
-            {
-                Log.d("TESTinfinity","${ngo.name}")
-                Log.d("TESTinfinity", "${ngoList.size}")
-            }
-            return ngoList
-        }
-
-        private suspend fun getListOfNgos(): MutableList<DocumentSnapshot> {
-            val snapshot = docRef.get().await()
-
-            return snapshot.documents
-        }
-    }
-
 
 }
--- App/app/src/main/java/com/gsoc2021/ngodonate/ui/browse/NGO.kt
+++ App/app/src/main/java/com/gsoc2021/ngodonate/ui/browse/NGO.kt
@@ -1,15 +1,10 @@
 package com.gsoc2021.ngodonate.ui.browse
 
-import android.content.Context
 import android.util.Log
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
-import kotlinx.coroutines.tasks.await
-import org.json.JSONException
-import org.json.JSONObject
-import java.lang.NullPointerException
 
 data class NGO(
     val name: String,
@@ -25,64 +20,29 @@ data class NGO(
         private var db = Firebase.firestore
         private lateinit var auth: FirebaseAuth
         val docRef = db.collection("NGOs")
-        val ngoList = ArrayList<NGO>()
+
         val documents: MutableList<DocumentSnapshot>? = null
 
-        suspend fun getNGOsFromFile(): ArrayList<NGO> {
-
-            val documents = getListOfNgos()
-
-            for(document in documents){
-                val email = document.get("email") as String
-                val current_ngo = NGO(
-                    document.get("name") as String,
-                    document.get("email") as String,
-                    document.get("phone_number") as String,
-                    document.get("hq_location") as String,
-                    document.get("volunteer") as String,
-                    document.get("website") as String
-                )
-                Log.d("EMAIL",  "$email" )
-                ngoList.add(current_ngo)
-            }
-
-
-            /*docRef.get()
-                .addOnCompleteListener { task ->
-                    if(task.isSuccessful)
-                    {
-                        for(document in task.result){
-                            val email = document.get("email") as String
-                            val current_ngo = NGO(
-                                document.get("name") as String,
-                                document.get("email") as String,
-                                document.get("phone_number") as String,
-                                document.get("hq_location") as String,
-                                document.get("volunteer") as String,
-                                document.get("website") as String
-                            )
-                            Log.d("EMAIL",  "$email" )
-                            ngoList.add(current_ngo)
-                        }
+        fun readData(myCallback: (ArrayList<NGO>) -> Unit) {
+            docRef.get().addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val ngoList = ArrayList<NGO>()
+                    for (document in task.result) {
+                        val email = document.get("email") as String
+                        val currentNgo = NGO(
+                            document.get("name") as String,
+                            document.get("email") as String,
+                            document.get("phone_number") as String,
+                            document.get("hq_location") as String,
+                            document.get("volunteer") as String,
+                            document.get("website") as String
+                        )
+                        Log.d("EMAIL", email)
+                        ngoList.add(currentNgo)
                     }
+                    myCallback(ngoList)
                 }
-                .addOnFailureListener { exception ->
-                    Log.d("FAILURE", "get failed with ", exception)
-                }
-                   */
-
-            for(ngo in ngoList)
-            {
-                Log.d("TESTinfinity","${ngo.name}")
-                Log.d("TESTinfinity", "${ngoList.size}")
             }
-            return ngoList
-        }
-
-        private suspend fun getListOfNgos(): MutableList<DocumentSnapshot> {
-            val snapshot = docRef.get().await()
-
-            return snapshot.documents
         }
     }
 }
