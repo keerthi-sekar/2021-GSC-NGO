b160d5554de387414c2555d4e79f2b42e694cd34
Kartikey <pandeykh@mail.uc.edu>
2021-03-06 00:09:28 -0500

    Added donate fragment

---
+++ App/app/src/main/java/com/gsoc2021/ngodonate/DonateActivity.kt
@@ -0,0 +1,11 @@
+package com.gsoc2021.ngodonate
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DonateActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_donate)
+    }
+}
