25b91ed3ff4a737673201c6220abcdd41c73979b
Keerth Sekar <keerthi.sekar101@gmail.com>
2021-03-27 14:14:32 -0400

    Add progressbar library

--- App/app/src/main/java/com/gsoc2021/ngodonate/ui/rewards/RewardsFragment.kt
+++ App/app/src/main/java/com/gsoc2021/ngodonate/ui/rewards/RewardsFragment.kt
@@ -4,15 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.gsoc2021.ngodonate.R
+import kotlinx.android.synthetic.main.fragment_rewards.*
+import kotlinx.android.synthetic.main.fragment_rewards.view.*
+import com.mackhartley.roundedprogressbar.RoundedProgressBar
 
 class RewardsFragment : Fragment() {
 
     private lateinit var rewardsViewModel: RewardsViewModel
+    private lateinit var roundedProgressBar: RoundedProgressBar
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,10 +24,19 @@ class RewardsFragment : Fragment() {
         rewardsViewModel =
             ViewModelProviders.of(this).get(RewardsViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_rewards, container, false)
-        val textView: TextView = root.findViewById(R.id.text_rewards)
+       /* val textView: TextView = root.findViewById(R.id.text_rewards)
         rewardsViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
-        })
+        })*/
+
+        val cPoints = 200
+        val mPoints = 500
+        val dPoints = "500"
+        //monthly_goal.text = dPoints
+        //setProgressBar(cPoints, mPoints)
+
         return root
     }
+
+
 }
