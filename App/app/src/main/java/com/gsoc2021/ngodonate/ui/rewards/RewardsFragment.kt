fd4776581ed47bb8806abbc744041e90653df224
Keerth Sekar <keerthi.sekar101@gmail.com>
2021-03-29 19:38:47 -0400

    Updated Code and xml
    Getting error: Could not find method getHours(View) in a parent or ancestor Context for android:onClick attribute defined on view class androidx.appcompat.widget.AppCompatButton with id 'hoursButton' for all buttons and Images

--- App/app/src/main/java/com/gsoc2021/ngodonate/ui/rewards/RewardsFragment.kt
+++ App/app/src/main/java/com/gsoc2021/ngodonate/ui/rewards/RewardsFragment.kt
@@ -4,16 +4,20 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import com.gsoc2021.ngodonate.R
 import kotlinx.android.synthetic.main.fragment_rewards.*
 import kotlinx.android.synthetic.main.fragment_rewards.view.*
 import com.mackhartley.roundedprogressbar.RoundedProgressBar
+import kotlinx.android.synthetic.*
 
 class RewardsFragment : Fragment() {
 
     private lateinit var rewardsViewModel: RewardsViewModel
+    private lateinit var hourConfirmationText: TextView
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,10 +31,17 @@ class RewardsFragment : Fragment() {
         rewardsViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })*/
-
+        hourConfirmationText = root.findViewById(R.id.hoursConfirm)
+        hourConfirmationText.text = " "
         return root
     }
-    fun swipeRight(view: View){
+
+    fun getHours(view: View)
+    {
+        hourConfirmationText.text = "Hours Received!"
+    }
+
+    fun clickRight(view: View){
         when(cSliderCardTitle.text){
             "NGO1" ->{
                 cSliderCardTitle.text = "NGO2"
@@ -49,7 +60,7 @@ class RewardsFragment : Fragment() {
             }
         }
     }
-    fun swipeLeft(view: View){
+    fun clickLeft(view: View){
         when(cSliderCardTitle.text){
             "NGO2" ->{
                 cSliderCardTitle.text = "NGO1"
